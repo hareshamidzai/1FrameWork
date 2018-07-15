dd56b9a35b2e1a526169612e7a71210da5a23fcf
CybertekSchool <hhamidz1100@gmail.com>
2018-07-15 15:19:39 -0400

    updated pom file

--- src/test/java/com/cbt/tests/TestBase.java
+++ src/test/java/com/cbt/tests/TestBase.java
@@ -7,18 +7,21 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import com.cbt.utilities.ConfigurationReader;
 import com.cbt.utilities.Driver;
 
 public class TestBase {
 	protected WebDriver driver;
-	Actions actions;
+    protected Actions actions;
 	
 	@BeforeMethod
 	public void setUp() {
 		driver = Driver.getDriver();
 		actions = new Actions(driver);
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.manage().window().fullscreen();
+		driver.manage().window().maximize();
+		
+		driver.get(ConfigurationReader.getProperty("url"));
 		
 		driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
 
